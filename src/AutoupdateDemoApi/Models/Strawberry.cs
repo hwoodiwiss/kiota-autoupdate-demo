2159a42fbc0beeaf96db69e7eb1a3a864679194a
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-04-10 00:44:38 +0100

    Add Strawberry Endpoints 🍓

---
+++ src/AutoupdateDemoApi/Models/Strawberry.cs
@@ -0,0 +1,9 @@
+namespace AutoupdateDemoApi.Models;
+
+public sealed class Strawberry
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public double NumberOfSeeds { get; set; }
+    public int Ripeness { get; set; }
+}
